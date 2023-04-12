41cfd4616af7614ba31c305a369902e75701c79e
Mr.Nayra <aryanedu5786@gmail.com>
2023-04-12 07:33:12 +0530

    first smart contract

---
+++ First Smart Contract/first.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.19;
+//Version of solidity that our code is written with
+
+contract Inbox {
+//Defines a new contract (structure is same as classes)
+    string public message;
+
+    constructor (string memory initialMessage) {
+    //constructor function
+        message = initialMessage;
+    }
+
+    function setMessage(string memory newMessage) public {
+        message = newMessage;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
